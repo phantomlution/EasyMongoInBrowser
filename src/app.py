e5da9c4db2f2fc6937af58f8b38577d30b362ed5
yixiaohuan <331835826@qq.com>
2021-05-29 22:24:48 +0800

    fix large request body issue

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-from flask import Flask
+from flask import Flask, request
 from routes.Mongo import mongo_api
 from routes.Proxy import proxy_api
 from routes.Selenium import selenium_api
@@ -15,6 +15,21 @@ app.register_blueprint(proxy_api)
 app.register_blueprint(selenium_api)
 
 app.config['SECRET_KEY'] = 'secret!'
+
+
+# fix large request body cause connection reset
+@app.before_request
+def handle_chunking():
+    """
+    Sets the "wsgi.input_terminated" environment flag, thus enabling
+    Werkzeug to pass chunked requests as streams.  The gunicorn server
+    should set this, but it's not yet been implemented.
+    """
+
+    transfer_encoding = request.headers.get("Transfer-Encoding", None)
+    if transfer_encoding == u"chunked":
+        request.environ["wsgi.input_terminated"] = True
+
 
 if __name__ == '__main__':
     port = 5001
